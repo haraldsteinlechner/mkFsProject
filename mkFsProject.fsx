ff8aaae55461b21f95db0cddab1cd0ae80f09040
Harald Steinlechner <hs@vrvis.at>
2015-03-03 13:25:39 +0100

    switched to my nuget.exe

--- mkFsProject.fsx
+++ mkFsProject.fsx
@@ -10,7 +10,7 @@ Directory.CreateDirectory "bin"
 printfn "downloading nuget.exe"
 let client = new WebClient ()
 client.CachePolicy <- new System.Net.Cache.RequestCachePolicy(System.Net.Cache.RequestCacheLevel.NoCacheNoStore);
-client.DownloadFile("https://raw.githubusercontent.com/vrvis/aardvark/master/bin/nuget.exe", @"bin/nuget.exe");
+client.DownloadFile("https://github.com/haraldsteinlechner/mkFsProject/raw/master/bin/nuget.exe", @"bin/nuget.exe");
 
 printfn "got nuget"
 
