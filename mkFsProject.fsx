6825e473e767eab7030c43455c29fbe5f779bc79
Harald Steinlechner <hs@vrvis.at>
2015-03-03 11:46:22 +0100

    bootstrap sync

--- mkFsProject.fsx
+++ mkFsProject.fsx
@@ -1,6 +1,7 @@
 ﻿
 open System.IO
 open System.Net
+open System.Diagnostics
 
 printfn "creating directory structure"
 Directory.CreateDirectory "packages"
@@ -8,7 +9,19 @@ Directory.CreateDirectory "tools"
 
 printfn "downloading nuget.exe"
 let client = new WebClient ()
-client.DownloadFile("https://github.com/vrvis/aardvark/tree/master/bin/nuget.exe", "tools");
+client.DownloadFile("https://github.com/vrvis/aardvark/tree/master/bin/nuget.exe", @".\tools");
 
 printfn "got nuget"
 
+printfn "bootstrapping (download)"
+client.DownloadFile("https://github.com/haraldsteinlechner/mkFsProject/blob/master/build.sh","build.sh");
+
+printfn "build file (download)"
+client.DownloadFile("https://github.com/haraldsteinlechner/mkFsProject/blob/master/build.fsx","build.fsx");
+
+let p = new Process()
+p.StartInfo.UseShellExecute <- false
+p.StartInfo.RedirectStandardOutput <- true
+p.StartInfo.FileName = "build.sh"
+p.Start()
+p.WaitForExit()
